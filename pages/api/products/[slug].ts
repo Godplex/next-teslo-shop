fcfcfbf00727ffef75655181e84bc2e0e86cd1eb
Julian Rincon <julian1918@hotmail.es>
2023-01-24 21:29:04 -0500

    Add empty value url

--- pages/api/products/[slug].ts
+++ pages/api/products/[slug].ts
@@ -44,7 +44,7 @@ const getProductsBySlug = async (
   product.images = product.images.map((image) => {
     return image.includes("http")
       ? image
-      : `${process.env.HOST_NAME}products/${image}`;
+      : `${process.env.HOST_NAME || ""}products/${image}`;
   });
 
   return res.status(200).json(product);
